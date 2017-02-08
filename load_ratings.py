6702748d5f2db8c6b28c67ec9fc34fd04c5df6d7
ghk829 <ghk829@gmail.com>
2017-02-08 17:12:24 +0900

    dir

--- load_ratings.py
+++ load_ratings.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sqlite3
-df=pd.read_csv("C:\Users\evalue\git\My_Turing\python\movie_lens\data\u.data",sep='\t',header=None)
+df=pd.read_csv("./data/u.data",sep='\t',header=None)
 df.columns=["user_id" , "item_id" , "rating" , "timestamp"]
-con = sqlite3.connect("C:\Users\evalue\git\My_Turing\python\movie_lens\db.sqlite3")
+con = sqlite3.connect("./db.sqlite3")
 df.to_sql("ratings_ratings",con=con,index=False,if_exists='append')
